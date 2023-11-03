a23f905c86aa8e784a2596b1cf25d51c9a731879
mrazomej <manuel.razo.m@gmail.com>
2023-11-03 11:36:55 -0700

    update test file to new module structure

--- test/infomaxvae.jl
+++ test/infomaxvae.jl
@@ -1,9 +1,8 @@
 ##
-println("Testing InforMaxVAEs module:\n")
+println("Testing InfoMaxVAEs module:\n")
 ##
 
-import Revise
-# Import AutoEncode.jl package
+# Import AutoEncode.jl module to be tested
 import AutoEncode.InfoMaxVAEs
 # Import Flux library
 import Flux
@@ -13,29 +12,21 @@ import Random
 import StatsBase
 
 Random.seed!(42)
-##
+
+## =============================================================================
 
 # Define number of inputs
 n_input = 3
 # Define number of synthetic data points
-n_data = 1_000
-# Define number of epochs
-n_epoch = 10_000
-# Define how often to compute error
-n_error = 100
-# Define batch size
-n_batch = 32
+n_data = 50
 # Define number of hidden layers
-n_hidden = 3
+n_hidden = 2
 # Define number of neurons in non-linear hidden layers
-n_neuron = 20
+n_neuron = 10
 # Define dimensionality of latent space
-latent_dim = 2
-# Define parameter scheduler
-epoch_change = [1, 10^4, 10^5, 5 * 10^5, 10^6]
-learning_rates = [10^-4, 10^-5, 10^-6, 10^-5.5, 10^-6];
+n_latent = 2
 
-##
+## =============================================================================
 
 println("Generating synthetic data...")
 
@@ -65,11 +56,11 @@ data = Matrix(hcat(x_rand, y_rand, z_rand)')
 dt = StatsBase.fit(StatsBase.ZScoreTransform, data, dims=2)
 
 # Center data to have mean zero and standard deviation one
-data = StatsBase.transform(dt, data)
+data = StatsBase.transform(dt, data);
 
-##
+## =============================================================================
 
-# Define VAE architecture
+println("Testing InfoMaxVAE = JointEncoder + SimpleDecoder")
 
 # Define latent space activation function
 latent_activation = Flux.identity
@@ -77,37 +68,50 @@ latent_activation = Flux.identity
 output_activation = Flux.identity
 
 # Define encoder layer and activation functions
-encoder = repeat([n_neuron], n_hidden)
+encoder_neurons = repeat([n_neuron], n_hidden)
 encoder_activation = repeat([Flux.swish], n_hidden)
 
 # Define decoder layer and activation function
-decoder = repeat([n_neuron], n_hidden)
+decoder_neurons = repeat([n_neuron], n_hidden)
 decoder_activation = repeat([Flux.swish], n_hidden)
 
 # Define MLP layer and activation function
-mlp = repeat([n_neuron], n_hidden)
+mlp_neurons = repeat([n_neuron], n_hidden)
 mlp_activation = repeat([Flux.swish], n_hidden)
 
 # Define MLP output activation function
 mlp_output_activation = Flux.identity
 
-##
-
-println("Testing VAE initialization and training...")
-
-# Initialize autoencoder
-infomaxvae = InfoMaxVAEs.infomaxvae_init(
+# Initialize encoder
+encoder = InfoMaxVAEs.JointEncoder(
     n_input,
-    latent_dim,
-    latent_activation,
-    output_activation,
-    encoder,
+    n_latent,
+    encoder_neurons,
     encoder_activation,
-    decoder,
+    latent_activation
+)
+
+# Initialize decoder
+decoder = InfoMaxVAEs.SimpleDecoder(
+    n_input,
+    n_latent,
+    decoder_neurons,
     decoder_activation,
-    mlp,
+    output_activation
+)
+
+# Initialize MLP
+mlp = InfoMaxVAEs.MLP(
+    n_input,
+    n_latent,
+    mlp_neurons,
     mlp_activation,
     mlp_output_activation
+)
+
+infomaxvae = InfoMaxVAEs.InfoMaxVAE(
+    InfoMaxVAEs.VAE(encoder, decoder),
+    mlp
 )
 
 # Test if it returns the right type
@@ -116,9 +120,7 @@ infomaxvae = InfoMaxVAEs.infomaxvae_init(
 ##
 
 # Test that reconstruction works
-@test isa(infomaxvae.vae(data; latent=false), AbstractVecOrMat)
-
-##
+@test isa(infomaxvae(data; latent=false), AbstractVecOrMat)
 
 # Generate list of permutated data
 data_shuffle = data[:, Random.shuffle(1:end)]
@@ -126,43 +128,30 @@ data_shuffle = data[:, Random.shuffle(1:end)]
 #  Test loss functions
 @test isa(
     InfoMaxVAEs.loss(
-        infomaxvae.vae, infomaxvae.mlp, data, data_shuffle
+        infomaxvae.vae, infomaxvae.mlp, data[:, 1], data_shuffle[:, 1]
     ),
     AbstractFloat
 )
 
 @test isa(
     InfoMaxVAEs.loss(
-        infomaxvae.vae, infomaxvae.mlp, data, data, data_shuffle
+        infomaxvae.vae, infomaxvae.mlp,
+        data[:, 1], data[:, 1], data_shuffle[:, 1]
     ),
     AbstractFloat
 )
 
 @test isa(
     InfoMaxVAEs.mlp_loss(
-        infomaxvae.vae, infomaxvae.mlp, data, data_shuffle
+        infomaxvae.vae, infomaxvae.mlp, data[:, 1], data_shuffle[:, 1]
     ),
     AbstractFloat
 )
 
-##
-
-@test isa(InfoMaxVAEs.mutual_info_mlp(infomaxvae, data), AbstractFloat)
-
-##
+## =============================================================================
 
 # Extract parameters
-params_init = deepcopy(
-    collect(
-        Flux.params(
-            infomaxvae.vae.encoder,
-            infomaxvae.vae.µ,
-            infomaxvae.vae.logσ,
-            infomaxvae.vae.decoder,
-            infomaxvae.mlp
-        )
-    )
-)
+params_init = deepcopy(collect(Flux.params(infomaxvae)))
 
 # Explicit setup of optimizer
 vae_opt = Flux.Train.setup(
@@ -176,26 +165,34 @@ mlp_opt = Flux.Train.setup(
 )
 
 # Loop through a couple of epochs
+losses = Float32[]  # Track the loss
+# Loop through a couple of epochs
 for epoch = 1:10
     # Test training function
     InfoMaxVAEs.train!(infomaxvae, data, vae_opt, mlp_opt)
+
+    # Compute average loss
+    push!(
+        losses,
+        StatsBase.mean(InfoMaxVAEs.loss.(
+            Ref(infomaxvae.vae), Ref(infomaxvae.mlp),
+            eachcol(data), eachcol(data_shuffle)
+        ))
+    )
 end # for
 
+# Check if loss is decreasing
+@test all(diff(losses) ≠ 0)
+
 # Extract modified parameters
-params_end = deepcopy(
-    collect(
-        Flux.params(
-            infomaxvae.vae.encoder,
-            infomaxvae.vae.µ,
-            infomaxvae.vae.logσ,
-            infomaxvae.vae.decoder,
-            infomaxvae.mlp
-        )
-    )
-)
+params_end = deepcopy(collect(Flux.params(infomaxvae)))
 
-
-@test all(params_init .!= params_end)
+# Check that parameters have significantly changed
+threshold = 1e-5
+# Check if any parameter has changed significantly
+@test all([
+    all(abs.(x .- y) .> threshold) for (x, y) in zip(params_init, params_end)
+])
 
 println("Passed tests for InfoMaxVAEs module!\n")
 ##
