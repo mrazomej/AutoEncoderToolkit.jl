e25d0023282b668208dab74c6d317f96676dee85
mrazomej <manuel.razo.m@gmail.com>
2024-03-31 19:30:41 -0700

    update list of tests to run

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,18 +2,36 @@ using AutoEncode
 using Test
 
 @testset "AutoEncode.jl" begin
+    # Test utils
+    include("utils.jl")
+
+    # Test encoders
+    include("encoders.jl")
+
+    # Test decoders
+    include("decoders.jl")
+
     # Test AE module
     include("ae.jl")
 
     # Test VAE module
     include("vae.jl")
 
+    # Test InfoVAE module
+    include("mmdvae.jl")
+
     # Test InfoMaxVAE module
     include("infomaxvae.jl")
 
     # Test HVAE module
-    include("hvae.jl")
+    # include("hvae.jl")
+
+    # Test RHVAE module
+    include("rhvae.jl")
+
+    # Test diffgeo module
+    include("diffgeo.jl")
 
     # Test regularization module
-    include("regularization.jl")
+    # include("regularization.jl")
 end
