5aeb810a9c06d2e2d7ff17d953fc7ff9b0ecd867
mrazomej <manuel.razo.m@gmail.com>
2024-04-29 14:24:39 -0700

    temporarily comment out other tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,23 +14,23 @@ using Test
     # Test AE module
     include("ae.jl")
 
-    # Test VAE module
-    include("vae.jl")
+    # # Test VAE module
+    # include("vae.jl")
 
-    # Test InfoVAE module
-    include("mmdvae.jl")
+    # # Test InfoVAE module
+    # include("mmdvae.jl")
 
-    # Test InfoMaxVAE module
-    include("infomaxvae.jl")
+    # # Test InfoMaxVAE module
+    # include("infomaxvae.jl")
 
-    # Test HVAE module
-    include("hvae.jl")
+    # # Test HVAE module
+    # include("hvae.jl")
 
-    # Test RHVAE module
-    include("rhvae.jl")
+    # # Test RHVAE module
+    # include("rhvae.jl")
 
-    # Test diffgeo module
-    include("diffgeo.jl")
+    # # Test diffgeo module
+    # include("diffgeo.jl")
 
     # Test regularization module
     # include("regularization.jl")
